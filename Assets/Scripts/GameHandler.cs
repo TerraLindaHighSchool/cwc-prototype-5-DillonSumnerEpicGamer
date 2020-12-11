d9117dc82fa1a26886ee95f999bba6518dfa0cf8
Dillon Sumner <56370697+DillonSumnerEpicGamer@users.noreply.github.com>
2020-12-11 14:47:18 -0800

    Lesson 5.1

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public List<GameObject> objPrefabs;
+
+    private float spawnRate = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnObj());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnObj()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, objPrefabs.Count);
+            Instantiate(objPrefabs[index]);
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRB;
+    private float minSpeed = 12.0f;
+    private float maxSpeed = 16.0f;
+    private float torque = 10.0f;
+    private float xRange = 4.0f;
+    private float yPos = -3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+
+        targetRB.AddForce(GetRandomForce(), ForceMode.Impulse);
+        targetRB.AddTorque(GetRandomTorque(), GetRandomTorque(), GetRandomTorque(), ForceMode.Impulse);
+
+        transform.position = GetRandomSpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter()
+    {
+        Destroy(gameObject);
+    }
+
+    Vector3 GetRandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float GetRandomTorque()
+    {
+        return Random.Range(-torque, torque);
+    }
+
+    Vector3 GetRandomSpawn()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), yPos);
+    }
+}
